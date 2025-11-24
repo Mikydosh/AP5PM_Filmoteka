0e084f7c3db5005074253541de61f1fc04327abd
Mikydosh <michal.dolik@email.cz>
2025-11-24 21:42:35 +0100

    Zobrazení filmů na kartě "Filmy"

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,20 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonSpinner } from '@ionic/angular/standalone';
+import { MovieService } from 'src/app/services/movie.service';
+import { Movie } from 'src/app/models/movie.model';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.page.html',
   styleUrls: ['./movies.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonSpinner]
 })
 export class MoviesPage implements OnInit {
 
-  constructor() { }
+  movies: Movie[] = [];
+  isLoading = true;
+
+  constructor(private movieService: MovieService) { }
 
   ngOnInit() {
+    this.loadMovies();
+  }
+
+  loadMovies(){
+    this.isLoading = true;
+    this.movieService.getPopular().subscribe({
+      next: (Response) => {
+        this.movies = Response.results;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error loading movies:', error);
+        this.isLoading = false;
+      }
+    });
+  }
+
+  getPosterUrl(path: string | null): string {
+    return this.movieService.getPosterUrl(path);
   }
 
 }
