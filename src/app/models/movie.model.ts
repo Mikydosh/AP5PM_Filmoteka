5a28530fc9f1425fbaa86d9c0205106a8eac52e6
Mikydosh <michal.dolik@email.cz>
2025-11-24 20:51:57 +0100

    Dodatek k API - struktura filmu (pro karty)

---
+++ src/app/models/movie.model.ts
@@ -0,0 +1,16 @@
+// Základní struktura filmu (pro karty)
+export interface Movie{
+    id: number;
+    title: string;
+    poster_path: string | null;
+    release_date: string;
+    vote_average: number;
+}
+
+// API odpověď
+export interface MovieResponse{
+    page: number;
+    results: Movie[];
+    total_pages: number;
+    total_results: number;
+}
