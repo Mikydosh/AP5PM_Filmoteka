013399f61ee27e543df81b66b66b19ab1004bbaa
Mikydosh <michal.dolik@email.cz>
2025-11-26 20:00:53 +0100

    Přidání filmu/seriálu do seznamů z detailu (přidáno tlačítko)

--- src/app/pages/movie-detail/movie-detail.page.ts
+++ src/app/pages/movie-detail/movie-detail.page.ts
@@ -1,22 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons, IonSpinner } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons, IonSpinner, IonButton, IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from 'src/app/services/movie.service';
 import { MovieDetail, Crew } from 'src/app/models/movie.model';
+// Přidání do seznamů
+import { ActionSheetController } from '@ionic/angular/standalone';
+import { StorageService } from '../../services/storage.service';
+import { MediaList } from '../../models/list.model';
+import { addIcons } from 'ionicons';
+import { bookmark, bookmarkOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.page.html',
   styleUrls: ['./movie-detail.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, IonBackButton, IonButtons, IonSpinner]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, IonBackButton, IonButtons, IonSpinner, IonButton, IonFab, IonFabButton, IonIcon ]
 })
+
+
 export class MovieDetailPage implements OnInit {
   movie: MovieDetail | null = null;
   isLoading = true;
 
-  constructor(private route: ActivatedRoute, private movieService: MovieService) { }
+  lists: MediaList[] = [];
+  isInLists: { [listId: string]: boolean } = {};
+
+  constructor(private route: ActivatedRoute, 
+    private movieService: MovieService, 
+    private storageService: StorageService, 
+    private actionSheetController: ActionSheetController 
+  ) { 
+    addIcons({ bookmark, bookmarkOutline });
+  }
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
@@ -28,8 +45,9 @@ export class MovieDetailPage implements OnInit {
   loadMovie(id: number) {
     this.isLoading = true;
     this.movieService.getDetail(id).subscribe({
-      next: (movie) => {
+      next: async (movie) => {
         this.movie = movie;
+        await this.loadLists();
         this.isLoading = false;
       },
       error: (error) => {
@@ -66,4 +84,124 @@ export class MovieDetailPage implements OnInit {
   getProfileUrl(path: string | null): string {
     return this.movieService.getProfileUrl(path);
   }
+
+
+  // Přidávání do seznamů
+  async loadLists() {
+    this.lists = await this.storageService.getAllLists();
+    
+    // Zkontroluj ve kterých seznamech už film je
+    if (this.movie) {
+      for (const list of this.lists) {
+        this.isInLists[list.id] = await this.storageService.isInList(
+          list.id,
+          this.movie.id,
+          'movie'
+        );
+      }
+    }
+  }
+
+  async openAddToListMenu() {
+    if (!this.movie) return;
+
+    const buttons = [];
+
+    // Přidej tlačítka pro existující seznamy
+    for (const list of this.lists) {
+      const isInList = this.isInLists[list.id];
+      buttons.push({
+        text: isInList ? `✓ ${list.name}` : list.name,
+        handler: async () => {
+          if (isInList) {
+            await this.removeFromList(list.id);
+          } else {
+            await this.addToList(list.id);
+          }
+        }
+      });
+    }
+
+  // Tlačítko pro vytvoření nového seznamu
+  buttons.push({
+    text: '+ Vytvořit nový seznam',
+    handler: async () => {
+      await this.createAndAddToList();
+    }
+  });
+
+  buttons.push({
+    text: 'Zrušit',
+    role: 'cancel'
+  });
+
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Přidat do seznamu',
+      buttons: buttons
+    });
+
+    await actionSheet.present();
+  }
+
+async addToList(listId: string) {
+  if (!this.movie) return;
+
+  await this.storageService.addToList(listId, {
+    id: this.movie.id,
+    title: this.movie.title,
+    poster_path: this.movie.poster_path,
+    type: 'movie',
+    vote_average: this.movie.vote_average,
+    release_date: this.movie.release_date,
+    addedAt: new Date()
+  });
+
+  await this.loadLists();
+}
+
+  async removeFromList(listId: string) {
+    if (!this.movie) return;
+
+    await this.storageService.removeFromList(listId, this.movie.id, 'movie');
+    await this.loadLists();
+  }
+
+  async createAndAddToList() {
+    const { AlertController } = await import('@ionic/angular/standalone');
+    const alertController = new AlertController();
+    
+    const alert = await alertController.create({
+      header: 'Nový seznam',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'Název seznamu'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Zrušit',
+          role: 'cancel'
+        },
+        {
+          text: 'Vytvořit',
+          handler: async (data) => {
+            if (data.name && data.name.trim().length > 0) {
+              const newList = await this.storageService.createList(data.name.trim());
+              await this.addToList(newList.id);
+            }
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  // Pro plnou/obrys ikony bookmarks (v seznamech)
+  isInAnyList(): boolean {
+  return Object.values(this.isInLists).some(value => value === true);
+}
+
 }
--- src/app/pages/series-detail/series-detail.page.ts
+++ src/app/pages/series-detail/series-detail.page.ts
@@ -1,33 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
-import {
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-  IonBackButton,
-  IonButtons,
-  IonSpinner
-} from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonBackButton, IonButtons, IonSpinner, IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
 import { SeriesService } from '../../services/series.service';
 import { SeriesDetail, Crew } from '../../models/series.model';
+// Přidání do seznamů
+import { ActionSheetController, IonButton } from '@ionic/angular/standalone';
+import { StorageService } from '../../services/storage.service';
+import { MediaList } from '../../models/list.model';
+import { addIcons } from 'ionicons';
+import { bookmark, bookmarkOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-series-detail',
   templateUrl: './series-detail.page.html',
   styleUrls: ['./series-detail.page.scss'],
   standalone: true,
-  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonBackButton, IonButtons, IonSpinner],
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonBackButton, IonButtons, IonSpinner, IonButton, IonFab, IonFabButton, IonIcon ],
 })
+
+
 export class SeriesDetailPage implements OnInit {
   series: SeriesDetail | null = null;
   isLoading = true;
 
+  lists: MediaList[] = [];
+  isInLists: { [listId: string]: boolean } = {};
+
   constructor(
     private route: ActivatedRoute,
-    private seriesService: SeriesService
-  ) {}
+    private seriesService: SeriesService,
+    private storageService: StorageService,
+    private actionSheetController: ActionSheetController
+  ) {
+    addIcons({ bookmark, bookmarkOutline });
+  }
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
@@ -39,9 +46,10 @@ export class SeriesDetailPage implements OnInit {
   loadSeries(id: number) {
     this.isLoading = true;
     this.seriesService.getDetail(id).subscribe({
-      next: (series) => {
+      next: async (series) => {
         this.series = series;
         console.log('Series data:', series);
+        await this.loadLists();
         this.isLoading = false;
       },
       error: (error) => {
@@ -84,4 +92,119 @@ export class SeriesDetailPage implements OnInit {
     }
     return `${this.series.episode_run_time[0]} min`;
   }
+
+  // Přidání do seznamu
+  async loadLists() {
+    this.lists = await this.storageService.getAllLists();
+    
+    if (this.series) {
+      for (const list of this.lists) {
+        this.isInLists[list.id] = await this.storageService.isInList(
+          list.id,
+          this.series.id,
+          'series'
+        );
+      }
+    }
+  }
+
+  async openAddToListMenu() {
+    if (!this.series) return;
+
+    const buttons = [];
+
+    for (const list of this.lists) {
+      const isInList = this.isInLists[list.id];
+      buttons.push({
+        text: isInList ? `✓ ${list.name}` : list.name,
+        handler: async () => {
+          if (isInList) {
+            await this.removeFromList(list.id);
+          } else {
+            await this.addToList(list.id);
+          }
+        }
+      });
+    }
+
+    buttons.push({
+      text: '+ Vytvořit nový seznam',
+      handler: async () => {
+        await this.createAndAddToList();
+      }
+    });
+
+    buttons.push({
+      text: 'Zrušit',
+      role: 'cancel'
+    });
+
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Přidat do seznamu',
+      buttons: buttons
+    });
+
+    await actionSheet.present();
+  }
+
+  async addToList(listId: string) {
+    if (!this.series) return;
+
+    await this.storageService.addToList(listId, {
+      id: this.series.id,
+      title: this.series.name,
+      poster_path: this.series.poster_path,
+      type: 'series',
+      vote_average: this.series.vote_average,
+      release_date: this.series.first_air_date,
+      addedAt: new Date()
+    });
+
+    await this.loadLists();
+  }
+
+  async removeFromList(listId: string) {
+    if (!this.series) return;
+
+    await this.storageService.removeFromList(listId, this.series.id, 'series');
+    await this.loadLists();
+  }
+
+  async createAndAddToList() {
+    const { AlertController } = await import('@ionic/angular/standalone');
+    const alertController = new AlertController();
+    
+    const alert = await alertController.create({
+      header: 'Nový seznam',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'Název seznamu'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Zrušit',
+          role: 'cancel'
+        },
+        {
+          text: 'Vytvořit',
+          handler: async (data) => {
+            if (data.name && data.name.trim().length > 0) {
+              const newList = await this.storageService.createList(data.name.trim());
+              await this.addToList(newList.id);
+            }
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+  
+  // Pro plnou/obrys ikony bookmarks (v seznamech)
+  isInAnyList(): boolean {
+  return Object.values(this.isInLists).some(value => value === true);
+}
 }
